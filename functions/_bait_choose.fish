03829d67d68f46d53f4e04464bbc262da721cdf4
Tim Harding <Tim@TimHarding.co>
2024-11-04 21:56:29 -0800

    Added choose

---
+++ functions/_bait_choose.fish
@@ -0,0 +1,31 @@
+function _bait_choose --argument-names key modifier
+    if not set -q _bait_state
+        set -g _bait_state 0
+        set -g _bait_options $argv
+    end
+
+    switch $key
+        case up
+            set _bait_state (math "($_bait_state - 1 + $(count $_bait_options))")
+        case down
+            set _bait_state (math "($_bait_state + 1)")
+        case enter
+            echo $_bait_options[(math "$_bait_state + 1")]
+            set -g _tackle_exit 0
+            return
+        case escape
+            set -g _tackle_exit 1
+            return
+    end
+    set _bait_state (math "$_bait_state % $(count $_bait_options)")
+
+    echo "Choose:"
+    for i in (seq (count $_bait_options))
+        if test (math "$_bait_state + 1") -eq $i
+            echo -n "> "
+        else
+            echo -n "  "
+        end
+        echo $_bait_options[$i]
+    end
+end
--- functions/bait.fish
+++ functions/bait.fish
@@ -1,6 +1,8 @@
 function bait --argument-names command
     switch $command
         case confirm
-            tackle _bait_confirm
+            tackle _bait_confirm $argv[2..]
+        case choose
+            tackle _bait_choose $argv[2..]
     end
 end
