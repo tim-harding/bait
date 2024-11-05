a6806265e5df44e0173c5c14536865ecce514265
Tim Harding <Tim@TimHarding.co>
2024-11-05 09:54:01 -0800

    Spinner

---
+++ functions/_bait_spin.fish
@@ -0,0 +1,39 @@
+function _bait_spin
+    for i in (seq (count $argv))
+        set arg $argv[$i]
+        if test $arg = --
+            set subcommand_index (math "$i + 1")
+            break
+        end
+    end
+
+    if not set -q subcommand_index
+        return
+    end
+
+    $argv[$subcommand_index..] &
+
+    if not set pid (jobs --last --pid)
+        return
+    end
+
+    function _bait_spin_handle_exit --on-process-exit $pid
+        set -g _bait_spin_exited 0
+    end
+
+    _tackle_cursor hide
+    while true
+        for c in ⡇ ⠏ ⠛ ⠹ ⢸ ⣰ ⣤ ⣆
+            if set -q _bait_spin_exited
+                break
+            end
+            echo -nes "$c loading..."
+            _tackle_cursor column 1
+            sleep 0.03
+        end
+        if set -q _bait_spin_exited
+            break
+        end
+    end
+    _tackle_cursor show
+end
--- functions/bait.fish
+++ functions/bait.fish
@@ -2,7 +2,11 @@ function bait --argument-names command
     switch $command
         case confirm
             tackle _bait_confirm $argv[2..]
+
         case choose
             tackle _bait_choose $argv[2..]
+
+        case spin
+            _bait_spin $argv[2..]
     end
 end
