84b7fe2f89f7aed6ccb1abb3331e5f53e74bc519
Tim Harding <Tim@TimHarding.co>
2024-11-06 15:33:50 -0800

    Don't use optional arguments in argparse

--- functions/_bait_style.fish
+++ functions/_bait_style.fish
@@ -1,5 +1,5 @@
 function _bait_style
-    if not argparse h/help 'border=?' 'align=?' 'margin=?' 'padding=?' 'style-border=?' 'style-text=?' -- $argv
+    if not argparse h/help 'border=' 'align=' 'margin=' 'padding=' 'style-border=' 'style-text=' -- $argv
         _bait_style_help
         return 1
     end
