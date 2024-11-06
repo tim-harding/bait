1e0e4f644e75063637ddde11be6451bb2d0fff4e
Tim Harding <Tim@TimHarding.co>
2024-11-05 16:10:53 -0800

    Bait input progress

--- functions/_bait_input.fish
+++ functions/_bait_input.fish
@@ -1,5 +1,6 @@
-function _bait_input
-    argparse h/help helptext 'placeholder=?' 'prompt=?' 'value=?' 'password=?' 'header=?' -- $argv
+function _bait_input -V BAIT_STYLE_PROMPT -V BAIT_STYLE_HEADER -V BAIT_STYLE_VALUE
+    argparse h/help password 'prompt=?' 'value=?' 'header=?' 'style-header=?' 'style-prompt=?' 'style-value=?' -- $argv
+    or return
 
     if set -q _flag_help
         echo -n "Usage: bait input [flags]
@@ -7,43 +8,47 @@ function _bait_input
 Prompt for some input
 
 Flags:
-  -h, --help                               Show context-sensitive help
-  -v, --version                            Print the version number
-      --placeholder='Type something...'    Placeholder value
-      --prompt=' >'                        Prompt to display
-      --value=''                           Initial value (can also be passed via stdin)
-      --password                           Mask input characters
-      --header=''                          Header value
-      --helptext                           Show help keybinds
+  -h, --help                     Show context-sensitive help
+      --prompt='> '              Prompt to display
+      --value=''                 Initial value (can also be passed via stdin)
+      --header=''                Header value
+      --password                 Mask input characters
+      --style-header='blue -o'   set_color flags for header
+      --style-prompt='black'     set_color flags for prompt
+      --style-value='normal'     set_color flags for value
 " &>stderr
         return
     end
 
-    set prompt_len (string length $_flag_prompt) 0
-    set prompt_len $prompt_len[1]
-    set line_count 1
-    set frame
+    set prompt $_flag_prompt "> "
+    set prompt $prompt[1]
+
+    set -q _flag_style_prompt
+    and set style_prompt $_flag_style_prompt
+    or set -q BAIT_STYLE_PROMPT
+    and set style_prompt $BAIT_STYLE_PROMPT
+    or set style_prompt black
+
+    set -q _flag_style_header
+    and set style_header $_flag_style_header
+    or set -q BAIT_STYLE_HEADER
+    and set style_header $BAIT_STYLE_HEADER
+    or set style_header blue -u
+
+    set -q _flag_style_value
+    and set style_value $_flag_style_value
+    or set -q BAIT_STYLE_VALUE
+    and set style_value $BAIT_STYLE_VALUE
+    or set style_value black
 
     if set -q _flag_header
-        set style $_bait_style_header (set_color normal)
-        set -a frame $style[1] $_flag_header \n
+        set_color $style_header
+        echo -es $_flag_header 1>&2
     end
 
-    if set -q _flag_placeholder
-        set style $_bait_style_placeholder (set_color black)
-        set -a frame (string repeat $prompt_len " ") $style[1] $_flag_placeholder
-    end
-
-    if set -q _flag_helptext
-        set style $_bait_style_helptext (set_color normal)
-        set -a frame $style[1] "\n\nenter: submit" (_tackle_cursor line-up 3)
-    end
-
-    echo -es $frame 1>&2
-    read input
-    if set -q _flag_helptext
-        _tackle_cursor line-down 3 1>&2
-        echo 1>&2
-    end
+    read --command "$_flag_value" \
+        (set -q _flag_password; and echo --silent) \
+        --prompt "set_color $style_prompt; echo -n '$prompt'; set_color $style_value" \
+        input
     echo $input
 end
