abcd550a0ca3702922a5b6fbd82fffc2c02f8932
Tim Harding <Tim@TimHarding.co>
2024-11-09 18:20:17 -0800

    Skip rebinding keys

--- functions/_bait_keybind.fish
+++ functions/_bait_keybind.fish
@@ -1,4 +1,7 @@
 function _bait_keybind
+    set -q _bait_did_bind_keys
+    and return
+
     for key in a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z \cA \cB \cC \cD \cE \cF \cG \cH \cI \cJ \cK \cL \cM \cN \cO \cP \cQ \cR \cS \cT \cU \cV \cW \cX \cY \cZ 0 1 2 3 4 5 6 7 8 9 \$ \\ \* \? \~ \# \( \) \{ \} \[ \] \< \> \& \| \; \" \'
         bind -M bait $key "_bait_key $key"
     end
@@ -17,6 +20,8 @@ function _bait_keybind
     bind -M bait \e "_bait_key escape"
     bind -M bait \e\[H "_bait_key home"
     bind -M bait \e\[F "_bait_key end"
+
+    set -g _bait_did_bind_keys
 end
 
 function _bait_key
