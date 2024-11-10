230e7a7a0b3f583825e15c40aa1e0b70a9257f80
Tim Harding <Tim@TimHarding.co>
2024-11-09 18:21:22 -0800

    Add redundant line of output

--- functions/_bait_ui_io.fish
+++ functions/_bait_ui_io.fish
@@ -1,5 +1,7 @@
 function _bait_ui_io
     echo -es $argv >/dev/tty
+    echo
+    bait cursor line-up 1
     read -g --prompt "bait cloak hide" _bait_ui_input
     bait cloak show
     bait cursor line-up (math 1 + (count (string join $argv))) >/dev/tty
