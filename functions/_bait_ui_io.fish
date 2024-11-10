eb67f7519d26a02832cf29c4ba0322afb3024e83
Tim Harding <Tim@TimHarding.co>
2024-11-09 18:24:26 -0800

    More tty piping

--- functions/_bait_ui_io.fish
+++ functions/_bait_ui_io.fish
@@ -1,7 +1,7 @@
 function _bait_ui_io
     echo -es $argv >/dev/tty
-    echo
-    bait cursor line-up 1
+    echo >/dev/tty
+    bait cursor line-up 1 >/dev/tty
     read -g --prompt "bait cloak hide" _bait_ui_input
     bait cloak show
     bait cursor line-up (math 1 + (count (string join $argv))) >/dev/tty
